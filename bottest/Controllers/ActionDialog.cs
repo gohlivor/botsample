aab5703cca6e966e098ff0c964caa138dd9c66f7
Nathalie <gohlivor@gmail.com>
2016-08-24 16:36:30 -0700

    Add project files.

---
+++ bottest/Controllers/ActionDialog.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Threading.Tasks;
+using AuthBot;
+using AuthBot.Dialogs;
+using AuthBot.Models;
+
+namespace bottest.Controllers
+{
+    [Serializable]
+    public class ActionDialog : IDialog<string>
+        //[LUISModel(Constants.putinluisappid, Constants.putLuissubsctiponid)]
+        //public class ActionDialog: LUISDialog<object>
+        //make task and tag it with [LUISIntent("task")]
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            // Just ignore this, all it does is guide the bot to the next method
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> msg)
+        {
+            try
+            {
+                // Here, we wait for the message. So that things can happen asynchronously, we have to wait for the user's input
+                IMessageActivity message = await msg;
+
+                // message.Text accesses what the user just said
+                // "await context.PostAsync" sends a string to the user asynchronously
+                await context.PostAsync(string.Format("You said {0}", message.Text));
+
+                // this just drives the dialog. when you're done with the current dialog instance (aka this ActionDialog's lifespan),
+                // you have to "Wait" for the method to finish.
+                context.Wait(MessageReceivedAsync); //end everything with this
+            }
+            catch (Exception e)
+            {
+                // Tell user something broke, and end this dialog
+
+                await context.PostAsync(string.Format("Crashed, {0}", e.Message));
+
+                context.Wait(MessageReceivedAsync);
+            }
+        }
+    }
+}
---
+++ bottest/Controllers/MessagesController.cs
@@ -0,0 +1,90 @@
+﻿using Autofac;
+using bottest.Controllers;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Internals;
+using Microsoft.Bot.Connector;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace bottest
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        [BotAuthentication]
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+
+            // TODOL tuyrn custo errors off or block all of them from output
+
+            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+            {
+                // calculate something for us to return
+                // TODO: generate response to message
+                try
+                {
+                    await Conversation.SendAsync(activity, () => new ActionDialog());
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+
+            else if (activity != null)
+            {
+                using (var scope = DialogModule.BeginLifetimeScope(Conversation.Container, activity))
+                {
+                    var client = scope.Resolve<IConnectorClient>();
+
+                    await client.Conversations.ReplyToActivityAsync(HandleSystemMessage(activity));
+                }
+            }
+
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+        }
+        
+        private Activity HandleSystemMessage(Activity message)
+
+        {
+
+            if (message.Type == ActivityTypes.Ping)
+
+            {
+
+                Activity reply = message.CreateReply();
+
+                reply.Type = ActivityTypes.Ping;
+
+                return reply;
+
+            }
+
+            else if (message.Type == ActivityTypes.ConversationUpdate && message.MembersAdded.Count != 0)
+
+            {
+
+                Activity reply = message.CreateReply();
+
+                reply.Type = ActivityTypes.Message;
+
+                reply.Text = string.Format("help message");
+
+                return reply;
+
+            }
+
+
+
+            return null;
+
+        }
+
+    }
+}
